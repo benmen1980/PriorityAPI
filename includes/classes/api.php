f94bb5a45a07374c806919dd1bba93582188fd42
elicheva katz <elisheva.g@simplyct.co.il>
2021-05-04 14:10:22 +0300

    add a Sortable User Registration Date Column to the All Users page

--- includes/classes/api.php
+++ includes/classes/api.php
@@ -215,6 +215,53 @@ class API
                 exit(json_encode(['status' => $response['status'], 'data' => $response['body'], 'headers' => $response['code'] . ' ' . $response['message']]));    
 
             });
+
+            /*
+            * Create a column. And maybe remove some of the default ones
+            * @param array $columns Array of all user table columns {column ID} => {column Name} 
+            */
+            add_filter( 'manage_users_columns',  function( $columns ) {
+            
+           
+            
+                // unset( $columns['posts'] ); // maybe you would like to remove default columns
+                $columns['registration_date'] = 'Registration date'; // add new
+            
+                return $columns;
+            
+            });
+            
+            /*
+            * Fill our new column with the registration dates of the users
+            * @param string $row_output text/HTML output of a table cell
+            * @param string $column_id_attr column ID
+            * @param int $user user ID (in fact - table row ID)
+            */
+            add_filter( 'manage_users_custom_column', function( $row_output, $column_id_attr, $user ){
+            
+                $date_format = 'j M, Y H:i';
+            
+                switch ( $column_id_attr ) {
+                    case 'registration_date' :
+                        return date( $date_format, strtotime( get_the_author_meta( 'registered', $user ) ) );
+                        break;
+                    default:
+                }
+            
+                return $row_output;
+            
+            }, 10, 3);
+
+            /*
+            * Make our "Registration date" column sortable
+            * @param array $columns Array of all user sortable columns {column ID} => {orderby GET-param} 
+            */
+            add_filter( 'manage_users_sortable_columns', function( $columns ) {
+            
+            
+                return wp_parse_args( array( 'registration_date' => 'registered' ), $columns );
+            });
+            
             
         });
 
